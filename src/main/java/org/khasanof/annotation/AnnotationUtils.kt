8f3270ea930bfca38f0029398fa8d5b23ef6864f
Nurislom <nurislomx72@gmail.com>
2023-09-23 22:56:26 +0500

    add new method for AnnotationUtils.kt #3

--- src/main/java/org/khasanof/annotation/AnnotationUtils.kt
+++ src/main/java/org/khasanof/annotation/AnnotationUtils.kt
@@ -2,8 +2,7 @@ package org.khasanof.annotation
 
 import org.khasanof.base.BaseUtils
 import java.lang.reflect.Method
-import java.util.Arrays
-import java.util.Objects
+import java.util.*
 
 fun hasAnnotation(method: Method, annotation: Class<out Annotation>, isSuper: Boolean): Boolean {
     BaseUtils.checkArgsIsNonNull(method, annotation, isSuper)
@@ -14,4 +13,27 @@ fun hasAnnotation(method: Method, annotation: Class<out Annotation>, isSuper: Bo
             }
     }
     return method.isAnnotationPresent(annotation)
+}
+
+fun hasAnnotation(method: Method, annotation: Class<out Annotation>): Boolean {
+    return method.isAnnotationPresent(annotation)
+}
+
+fun getAnnotation(method: Method, annotation: Class<out Annotation?>, isSuper: Boolean): Annotation? {
+    BaseUtils.checkArgsIsNonNull(method, annotation, isSuper)
+    return if (isSuper) {
+        Arrays.stream(method.annotations)
+            .filter { f: Annotation? ->
+                f?.javaClass?.componentType()!!.isAnnotationPresent(annotation)
+            }
+            .findFirst().orElse(null)
+    } else method.getAnnotation(annotation)
+}
+
+fun getAnnotation(method: Method, annotation: Class<out Annotation>): Annotation? {
+    return Arrays.stream(method.annotations)
+        .filter {
+            it.javaClass.componentType.equals(annotation)
+        }
+        .findFirst().orElse(null)
 }
